0b3fdec00271dbeb45576eacbfca6e5646b42b81
Prajwal <prajwalwankhede890@gmail.com>
2025-09-22 19:54:13 +0530

    Ecommerce data modeling

---
+++ 4. E-commerce and Hospital mangement data modelling/E-commerce/model/product.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const productSchema = new mongoose.Schema({
+       
+    desciptions :{
+        required :  true ,
+        type : String ,
+    },
+    name :{
+        required :  true ,
+        type : String ,
+    },
+    productImage : {
+
+        type : String ,
+         
+    },
+    price :{
+        type : Number,
+        default : 0 ,
+
+    },
+    stock :{
+        type : Number,
+        default : 0 
+        
+    },
+    category : {
+        type : mongoose.Schema.Types.ObjectId ,
+        ref : "Category" ,
+        required : true ,
+    },
+    owner : {
+        type : mongoose.Schema.Types.ObjectId ,
+        ref : "User",
+    }
+
+
+},{timestamps : true})
+
+export const Product  = mongoose.model("Product", productSchema)
